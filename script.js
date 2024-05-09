be76dbc69fd848c2f51ec7a0f10fac3273a1f369
iamgagana <72552175+iamgagana@users.noreply.github.com>
2024-05-09 17:50:18 +0530

    Add files via upload
    using if else block

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const firstName = 'Jonas';
-const job = 'teacher';
-const birthYear = 1991;
-const year = 2037;
-const jonas = "I'm" + " " + firstName + ',a' + " " + (year - birthYear) + " " + 'year old' + job + '!';
-console.log(jonas);
-/*this one we have declared string but the format is not comfortable so we are using string template
-with template literals we can write a string in a more normal way and then basically insert the variables directly into the string and then they will simply be replaced
-so basically a template literals can assemble multiple pieces into one final string*/
-const jonasNew = `Im ${firstName} , a ${year - birthYear} year old ${job}! `
-console.log(jonasNew);
+const age = 19;
+const isOldEnough = age >= 18;
+if (isOldEnough) {
+    console.log(`Sarah can start driving License`);
+}
+
+const age1 = 15;
+if (age1 >= 18) {
+    console.log(`Sarah can start driving license`);
+} else {
+    const yearleft = 18 - age1;
+    console.log(`Sarah is too young wait another ${yearleft} years:`);
+}
+
+const birthYear = 2021;
+let century;
+if (birthYear <= 2000) {
+    let century = 20;
+} else {
+    let century = 21;
+}
+console.log(century);
