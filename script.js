998125a8397793ddf10460810cb6ded7e5ec8bd4
iamgagana <72552175+iamgagana@users.noreply.github.com>
2024-05-09 16:59:39 +0530

    Add files via upload
    first scripting program practised

---
+++ script.js
@@ -0,0 +1,31 @@
+
+let js = "amazing";
+if (js === 'amazing')
+    alert('Javascript is fun')
+let country = "India";
+let continent = "Asia";
+console.log(country);
+console.log(continent);
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+let firstName = "Matilda";
+console.log(firstName);
+let jona_matilda = "JM";
+let $function = 27;
+let person = "jona";
+let PI = 3.1415;
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+let job1 = "programmer";
+let job2 = "teacher";
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+let year;
+console.log(year);
+console.log(typeof year);
